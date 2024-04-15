45db9f534170687a9ccc1206796585029d83eb41
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:59:22 -0300

    feat: filtro de usuario por id

--- src/server.js
+++ src/server.js
@@ -2,6 +2,28 @@
 const express = require('express')
 const app = express()
 
+const lista = [
+    {
+        id: '1',
+        nome: 'amanda',
+    },
+    {
+        id: '2',
+        nome: 'patrick',
+    },
+    {
+        id: '3',
+        nome: 'ygor',
+    },
+]
+
+app.get('/lista/:id', async (req, res) => {
+    return res.status(200).json({
+        lista: lista.filter((usuario) => usuario.id === req.params.id)
+    })
+})
+
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
