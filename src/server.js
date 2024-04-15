746f0a06c1e15dfe7969f2ccec94e4a0ff18d4f4
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:57:35 -0300

    feat: adicionar listagem

--- src/server.js
+++ src/server.js
@@ -2,6 +2,25 @@
 const express = require('express')
 const app = express()
 
+const lista = [
+    {
+        id: '1',
+        nome: 'amanda',
+    },
+    {
+        id: '2',
+        nome: 'patrick',
+    },
+    {
+        id: '3',
+        nome: 'ygor',
+    },
+]
+
+app.get('/lista', async (res, req)=> {
+    return res.status(200).json({lista})
+})
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
