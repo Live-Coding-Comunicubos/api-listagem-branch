bef82d3c96b774e14952938a27390d848f658a23
Amandalfs <amandalfs57@gmail.com>
2024-04-15 15:58:47 -0300

    feat: adicionar criar usuario e excluir usuario

--- src/server.js
+++ src/server.js
@@ -2,6 +2,36 @@
 const express = require('express')
 const app = express()
 
+const lista = [
+    {
+        id: '1',
+        nome: 'amanda',
+    },
+    {
+        id: '2',
+        nome: 'patrick',
+    },
+    {
+        id: '3',
+        nome: 'ygor',
+    },
+]
+
+app.post("/lista", (req, res)=> {
+    lista.push({
+        id: (lista.length) + 2,
+        nome: 'Novo Usuario'
+    })
+    return res.status(201).json({ texto:"novo usuario adicionado"})
+})
+
+
+app.delete("/lista/:id", (req, res)=> {
+    const indexUsuario = lista.findIndex((usuario) => usuario.id === id);
+    lista.splice(indexUsuario, 1);
+    return res.status(200).json({ texto:"usuario excluido"})
+})
+
 app.listen(3000, ()=>{ 
     console.log('servidor rodando na porta 3000')
 })
